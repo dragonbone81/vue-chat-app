8b5aec86a4e36dbaeca9f789de9f2fb9aabf8784
Christian <cvernikoff@gmail.com>
2018-09-04 10:14:56 -0700

    some simple bulma + text input before nodejs server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="home">
+    <div class="home section">
+        <div class="container is-fluid">
+            <div class="columns is-centered">
+                <div class="column has-text-centered chat-box">
+                    <h2 class="title is-3">Chat</h2>
+                    <b-input v-model="name"/>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import BInput from "buefy/src/components/input/Input";
+
     export default {
         name: 'home',
-        components: {}
+        components: {
+            BInput,
+        }
     }
 </script>
+<style scoped>
+    .chat-box {
+        border: 2px solid green;
+        max-width: 800px;
+    }
+</style>
